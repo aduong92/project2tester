99f00a3b94235f9e7dd067a1c81b67338478d7ca
Anh Duong <a.dng223@gmail.com>
2019-03-30 02:13:25 -0500

    made changes to express

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 // Dependencies
-var http = require("http");
-var fs = require("fs");
+var express = require("express");
+var path = require("path");
+
 
 // Set our port to 8080
+var app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
 // Create our server
-var server = http.createServer(handleRequest);
+app.get('/jsQr.js', function(req, res) {
+    res.sendFile(path.join(__dirname + '/jsQr.js'));
+});
 
-// Create a function for handling the requests and responses coming into our server
-function handleRequest(req, res) {
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-  // Here we use the fs package to read our index.html file
-  fs.readFile(__dirname + "/index.html", function(err, data) {
 
-    // We then respond to the client with the HTML page by specifically telling the browser that we are delivering
-    // an html file.
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(data);
-  });
-}
-
-// Starts our server
-server.listen(PORT, "0.0.0.0", function() {
-  console.log("Server is listening on PORT: " + PORT);
+app.listen(PORT, "0.0.0.0",function() {
+  console.log("App listening on PORT " + PORT);
 });
